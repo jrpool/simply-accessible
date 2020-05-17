b4e129e8fbaf025082e603295e97361de257f870
Jonathan Pool <pool@stulta.com>
2020-05-16 22:12:08 -0400

    Debugged tall table.

--- tables/src/makestd.mjs
+++ tables/src/makestd.mjs
@@ -1,5 +1,19 @@
+// Function to return an identified element.
+const elementOf = (doc, selectorOrElement) => {
+  let element;
+  if (typeof selectorOrElement === 'string') {
+    element = doc.querySelector(selectorOrElement);
+  }
+  else {
+    element = selectorOrElement;
+  }
+  return element;
+};
+
 // Function to insert an element into a document.
-const insertEl = (doc, selector, isAtStart, type, textContent, attributes) => {
+const insertEl = (
+  doc, selectorOrElement, isAtStart, type, textContent, attributes
+) => {
   const newEl = doc.createElement(type);
   if (textContent) {
     newEl.textContent = textContent;
@@ -7,28 +21,58 @@ const insertEl = (doc, selector, isAtStart, type, textContent, attributes) => {
   Object.keys(attributes).forEach(key => {
     newEl.setAttribute(key, attributes[key]);
   });
-  doc.querySelector(selector).insertAdjacentElement(
+  elementOf(doc, selectorOrElement).insertAdjacentElement(
     isAtStart ? 'afterbegin' : 'beforeend', newEl
   );
   return newEl;
 };
 
 // Function to insert attributes into an element.
-const insertAtts = (doc, selector, attributes) => {
-  const el = doc.querySelector(selector);
+const insertAtts = (doc, selectorOrElement, attributes) => {
+  const element = elementOf(doc, selectorOrElement);
   Object.keys(attributes).forEach(key => {
-    el.setAttribute(key, attributes[key]);
+    element.setAttribute(key, attributes[key]);
   });
 };
 
+// Function to insert a meta element into the head.
+const insertMeta = (doc, src, name) => {
+  if (src[name]) {
+    insertEl(doc, 'head', false, 'meta', null, {
+      name,
+      content: src[name]
+    });
+  }
+};
+
+// Function to populate the document with standard content.
 const makeStd = (src, doc) => {
   insertAtts(doc, 'html', {'lang': 'en-US'});
-  insertEl(doc, 'head', true, 'meta', null, {
-    'name': 'viewport',
-    'content': 'width=device-width, initial-scale=1'
+  insertEl(doc, 'head', false, 'meta', null, {
+    name: 'viewport',
+    content: 'width=device-width, initial-scale=1'
   });
-  insertEl(doc, 'head', true, 'title', src.title, {});
+  insertMeta(doc, src, 'author');
+  if (src.authorlink) {
+    insertEl(doc, 'head', false, 'link', null, {
+      'rel': 'author',
+      'href': src.authorlink
+    });
+  }
+  insertMeta(doc, src, 'creator');
+  insertMeta(doc, src, 'publisher');
+  insertMeta(doc, src, 'description');
+  insertMeta(doc, src, 'keywords');
+  insertEl(doc, 'head', false, 'title', src.titleh1, {});
+  insertEl(doc, 'body', false, 'main', null, {});
+  insertEl(doc, 'main', false, 'header', null, {});
+  insertEl(doc, 'header', false, 'h1', src.titleh1, {});
+  insertEl(doc, 'main', false, 'table', null, {});
+  if (src.caption) {
+    insertEl(doc, 'table', false, 'caption', src.caption, {});
+  }
+  insertEl(doc, 'table', false, 'thead', null, {});
+  insertEl(doc, 'table', false, 'tbody', null, {});
 };
 
-// module.exports = {insertEl, insertAtts, makeStd};
 export {insertEl, insertAtts, makeStd};
--- tables/src/maketall.mjs
+++ tables/src/maketall.mjs
@@ -1,17 +1,37 @@
+/*
+  maketall.mjs
+  Use a specified table-definition file to create a tall complex table.
+  Write an HTML document containing the table to a specified file.
+  Arguments:
+    0: table-definition file.
+    1: output file.
+*/
+
 // Imports
 import fs from 'fs';
 import jsdom from 'jsdom';
 const {JSDOM} = jsdom;
+import pretty from  'pretty';
 import {insertEl, insertAtts, makeStd} from './makestd.mjs';
 
-// Extract an object from the source file.
 const args = process.argv.slice(2);
+
+// Validate the output file.
+let destFilePath;
+if (args.length === 2 && args[1].endsWith('.html')) {
+  destFilePath = args[1];
+}
+else {
+  throw new Error('Path of .html destination file is required argument 1.');
+}
+
+// Extract an object from the source file.
 let srcFilePath;
 if (args.length && args[0].endsWith('.json')) {
   srcFilePath = args[0];
 }
 else {
-  throw new Error('Path of .json source file a required argument.');
+  throw new Error('Path of .json source file is required argument 0.');
 }
 const srcString = fs.readFileSync(srcFilePath, 'utf8');
 let src;
@@ -27,15 +47,88 @@ else {
 const dom = new JSDOM('<!DOCTYPE html>');
 const {window} = dom;
 const doc = window.document;
-/*
-console.log(
-  JSON.stringify(
-    Object.getOwnPropertyNames(Object.getPrototypeOf(doc)).sort(), null, 2
-  )
-);
-*/
 
-// Add content to it.
+// Add standard content to it.
 makeStd(src, doc);
 
-console.log(JSON.stringify(doc.documentElement.outerHTML));
+// Add a stylesheet reference to it.
+insertEl(doc, 'head', false, 'link', null, {
+  type: 'text/css',
+  rel: 'stylesheet',
+  href: 'tall-style.css'
+});
+
+// Function to process a category.
+const handleCat = (key, val, IDMap, rowGroupIDs) => {
+  console.log(
+    'Starting handleCat on ' + key + ' with ' + rowGroupIDs.join('+')
+  );
+  const row = insertEl(doc, 'tbody', false, 'tr', null, {});
+  if (Array.isArray(val)) {
+    insertEl(doc, row, false, 'th', key, {id: `hd${++lastID}`});
+    const headersStart = rowGroupIDs
+    .concat(lastID)
+    .map(ID => `hd${ID}`)
+    .join(' ');
+    console.log('headersStart is ' + headersStart);
+    for (let i = 0; i < val.length; i++) {
+      const headersEnd = IDMap[i + 1].map(ID => `hd${ID}`).join(' ');
+      insertEl(
+        doc,
+        row,
+        false,
+        'td',
+        val[i],
+        {headers: `${headersStart} ${headersEnd}`}
+      )
+    }
+  }
+  else {
+    insertEl(doc, row, false, 'th', key, {
+      id: `hd${++lastID}`,
+      colspan: src.schema.facts.colCount,
+      class: `rank${rowGroupIDs.length}`
+    });
+    const lID = lastID;
+    Object.keys(val).forEach(key => {
+      handleCat(key, val[key], IDMap, rowGroupIDs.concat(lID));
+    });
+  }
+};
+
+// Add a tall table to it.
+let lastID = -1;
+const IDMap = [];
+const facts = src.schema.facts;
+const headerSpecs = facts.headerSpecs;
+headerSpecs.forEach(rowSpecs => {
+  const tr = insertEl(doc, 'thead', false, 'tr', null, {});
+  rowSpecs.forEach(cellSpecs => {
+    const cell = insertEl(doc, tr, false, 'th', cellSpecs[0], {});
+    if (cellSpecs[1] > 1) {
+      insertAtts(doc, cell, {rowspan: cellSpecs[1]});
+    }
+    if (cellSpecs[2] > 1) {
+      insertAtts(doc, cell, {colspan: cellSpecs[2]});
+    }
+    if (cellSpecs[3].length) {
+      insertAtts(doc, cell, {id: `hd${++lastID}`});
+      cellSpecs[3].forEach(col => {
+        if (IDMap[col]) {
+          IDMap[col].push(lastID);
+        }
+        else {
+          IDMap[col] = [lastID];
+        }
+      });
+    }
+  });
+});
+const data = src.data;
+Object.keys(data).forEach(key => {
+  handleCat(key, data[key], IDMap, []);
+});
+
+// Output the HTML of the document.
+fs.writeFileSync(destFilePath, pretty(dom.serialize()));
+console.log(`File ${destFilePath} written.`);
